a91cd40b600cffa0a007f50c7e49da21ad86e030
Artemi Bogatov <aboga99@mail.ru>
2025-03-22 17:45:01 +0300

    Исправление структуры проекта.

--- config/settings.py
+++ config/settings.py
@@ -1,11 +1,16 @@
 from pathlib import Path
-
+import os
+from dotenv import load_dotenv
 from django.conf.global_settings import STATICFILES_DIRS
+
+
+load_dotenv(override=True)
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-SECRET_KEY = 'django-insecure-lzb&j3g3ax=k%5)iyi$ipb9sy(h%1a(!e(sy63cw-8*k^s4j6*'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 
 DEBUG = True
@@ -57,8 +62,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('NAME'),
+        'USER': os.getenv('USER'),
+        'PASSWORD': os.getenv('PASSWORD'),
+        'HOST': os.getenv('HOST'),
+        'PORT': os.getenv('PORT'),
     }
 }
 
--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # Все пути из приложения catalog будут доступны по корню сайта
     path('', include('catalog.urls')),
 ]
